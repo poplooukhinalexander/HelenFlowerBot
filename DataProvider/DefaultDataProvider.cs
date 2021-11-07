81cc2b14f46083c434ae16acbb5c149f6ef7b713
Poploukhin Aleksandr <poplooukhin@gmail.com>
2021-11-08 00:57:29 +0300

    Удалил стабы из провайдера данных.

--- DataProvider/DefaultDataProvider.cs
+++ DataProvider/DefaultDataProvider.cs
@@ -14,23 +14,6 @@ namespace DataProvider
 			var botDataFileContent = File.ReadAllText(botDataFilePath);
 			var botData = JsonConvert.DeserializeObject<BotData>(botDataFileContent);
 			return botData;
-		}
-		public string GetHelloText(string username)
-		{
-			return @$"{username}, здравствуйте\!
-
-Вам пишет телеграм\-бот Елены Поплоухиной\. Я очень хочу принести пользу\. Пожалуйста, выберите продукт для скачивания\.";
-		}	
-		
-		public IEnumerable<ProductItem> GetProducts()
-		{
-			return new ProductItem[] 
-			{
-				new ProductItem { Name = "Шпаргалка для беременных", Link = "https://drive.google.com/drive/folders/1oMHWRBEa86XNnW9M6k0ZKMk7ioG1BgMt?usp=sharing" },
-				new ProductItem { Name = "Аптечка для новорожденного", Link = "https://drive.google.com/drive/folders/1cwtv0spylpX5nJxvdh9JYleJrzhEYVjh?usp=sharing" },
-				new ProductItem { Name = "Товары для новорожденных ", Link = "https://www.lenatheflower.com/newborn" },
-				new ProductItem { Name = "Instagram-блоги для мам", Link = "https://www.instagram.com/lenatheflower/guide/_/17872493435553673/" },
-			};
-		}
+		}		
 	}
 }
