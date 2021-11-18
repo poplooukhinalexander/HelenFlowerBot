eefb4896d71a0725119ac626314ede0b3080d57e
Poploukhin Aleksandr <poplooukhin@gmail.com>
2021-11-18 22:03:23 +0300

    Добавил демо-проект для AWS S3.

--- DataProvider/AwsS3DataProvider.cs
+++ DataProvider/AwsS3DataProvider.cs
@@ -6,14 +6,14 @@ using System.Threading.Tasks;
 namespace DataProvider
 {
 	public class AwsS3DataProvider : BaseDataProvider
-	{
-		private readonly AmazonS3Client s3Client = new AmazonS3Client();
+	{		
 		protected override async Task<string> LoadBotDataAsync(CancellationToken cancellationToken)
 		{
+			using var s3Client = new AmazonS3Client();
 			using var s3Object = await s3Client.GetObjectAsync("helen-bot-data", "BotData.json", cancellationToken);
 			using var sr = new StreamReader(s3Object.ResponseStream);
 			var botRawData = await sr.ReadToEndAsync();
-			return botRawData;
+			return botRawData;			
 		}
 	}
 }
---
+++ S3Demo/Program.cs
@@ -0,0 +1,20 @@
+﻿using DataProvider;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace S3Demo
+{
+	class Program
+	{
+		static async Task Main(string[] args)
+		{
+			var dataProvider = new AwsS3DataProvider();
+			var cts = new CancellationTokenSource();
+			var data = await dataProvider.GetBotDataAsync(cts.Token);
+			Console.WriteLine(data.HelloText);
+			Console.ReadLine();
+			cts.Cancel();
+		}
+	}
+}
